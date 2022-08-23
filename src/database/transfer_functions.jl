1e10ab3cbdcd0cf395fe585098a17753d00f04e7
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-08-23 15:40:28 -0300

    :sparkles: Add transfer function `tf_uint32`

--- src/database/transfer_functions.jl
+++ src/database/transfer_functions.jl
@@ -7,7 +7,7 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export tf_uint8, tf_uint16
+export tf_uint8, tf_uint16, tf_uint32
 
 """
     tf_uint8(raw::AbstractVector{UInt8})
@@ -25,4 +25,13 @@ Convert the raw telemetry into an `UInt16`.
 """
 function tf_uint16(raw::AbstractVector{UInt8})
     return reinterpret(UInt16, raw[begin:begin+1]) |> first
+end
+
+"""
+    tf_uint32(raw::AbstractVector{UInt8})
+
+Convert the raw telemetry into an `UInt32`.
+"""
+function tf_uint32(raw::AbstractVector{UInt8})
+    return reinterpret(UInt32, raw[begin:begin+1]) |> first
 end
